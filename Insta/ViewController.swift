6498726a4fa5a48d685a1754074921464915e363
milstetsenko <milstetsenko>
2020-10-23 17:48:38 +0100

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Milana Stetsenko on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
