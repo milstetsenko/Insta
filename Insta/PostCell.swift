c1036b49fa58f17f640880a2e2a3c255ee21859b
milstetsenko <milstetsenko>
2020-10-23 20:01:03 +0100

    Creating the feed

---
+++ Insta/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Insta
+//
+//  Created by Milana Stetsenko on 10/23/20.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
