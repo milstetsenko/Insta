9d4ef237ce75ba25539a19d927eac99f45de9817
milstetsenko <milstetsenko>
2020-10-23 18:47:33 +0100

    Sign in feature

--- Insta/LoginViewController.swift
+++ Insta/LoginViewController.swift
@@ -22,7 +22,16 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func onSignIn(_ sender: Any) {
-      
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password:password)
+        { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error \(String(describing: error)) ")
+            }
+        }
 
     }
     
